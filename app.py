f75f6bd5b22f590d2e1658975a9fa6015464be13
Michael Humbert <michael.t.humbert@gmail.com>
2020-06-17 16:40:52 -0500

    request edit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
+@app.route('/',methods=['get'])
 def index():
   #return render_template('index.html')
   ticker=request.form('Stock Ticker')

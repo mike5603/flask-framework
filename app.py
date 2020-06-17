594978f7ae93667be0689b29bdb7c01e1c818a94
Michael Humbert <michael.t.humbert@gmail.com>
2020-06-17 16:46:13 -0500

    request edit

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+data = {}
 
 @app.route('/',methods=['get'])
 def index():
   #return render_template('index.html')
-  ticker=request.form('Stock Ticker')
-  return ticker
+  data['ticker']=request.args('Stock Ticker')
+  return data['ticker']
 @app.route('/about')
 def about():
   return render_template('about.html')

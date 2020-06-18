2f1bc25fc0c96a952958edf5b82f18a709e376f6
Michael Humbert <michael.t.humbert@gmail.com>
2020-06-18 17:32:45 -0500

    test

--- app.py
+++ app.py
@@ -24,12 +24,13 @@ def index():
   df = df.sort_index()
   df_range = df.loc['2020-05-01':'2020-06-01']
   df_range['Date'] = df_range.index
+  df_range['Date_str'] = df_range.index.strftime('%Y-%m-%d')
   df_range = df_range.rename(columns={'1. open':'open','2. high':'high','3. low':'low','4. close':'close','5. volume':'volume'})
   p=figure(x_axis_type='datetime')
   p.xaxis.axis_label='Date'
   p.yaxis.axis_label='Closing Price'
   p.line(x='Date',y='close', source=df_range)
-  p.add_tools(HoverTool(tooltips=[('Date','@Date'),('Closing Value',"@close")],formatters={'@Date':'datetime'}))
+  p.add_tools(HoverTool(tooltips=[('Date','@Date_str'),('Closing Value',"@close")]))
   htmlo =open('templates/plot.html','w')
   htmlo.write(file_html(p,CDN,'Stock Output'))
   htmlo.close()

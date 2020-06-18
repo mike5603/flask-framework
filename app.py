014ee30a247b2a1462fc87cf409c0ab9e7285333
Michael Humbert <michael.t.humbert@gmail.com>
2020-06-18 10:48:29 -0500

    test

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect
+import requests
+import json
+import pandas
 
 app = Flask(__name__)
 app.vars = {}
@@ -9,7 +12,10 @@ def index():
   if request.method=='GET':
     return render_template('input.html')
   app.vars['ticker']=request.form['Stock Ticker']
-  return app.vars['ticker']
+  r = requests.get('https://www.alphavantage.co/query',params={'function':'TIME_SERIES_DAILY','symbol':app.vars['ticker'],'apikey':'MB1WQJ87O5O9N9WM'})
+  data = json.loads(r.text)
+  df = pandas.DataFrame.from_dict(data['Time Series (Daily)'])
+  return df.head
 
 
 @app.route('/about')

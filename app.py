10d3aa7d24fa9859741570ae70fd97a500b5bb01
Michael Humbert <michael.t.humbert@gmail.com>
2020-06-19 09:35:54 -0500

    test

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def about():
   return render_template('about.html')
 
 if __name__ == '__main__':
-  app.run(debug=true)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
